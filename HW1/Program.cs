b00c66524226c8c2b68244bd269c18558cb0d338
cuong1012006 <cuong1012006az@gmail.com>
2025-09-11 20:52:53 +0700

    HW1 code done

---
+++ HW1/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Nguyễn Xuân Cương \t12424043\t12424043\tcuong1012006\tcuong1012006@gamil.com");
+    }
+}
