22b3ae9e0c5591c825232f79fd991a68a70f70d6
cuong1012006 <cuong1012006az@gmail.com>
2025-10-10 10:36:12 +0700

    Create motor.cs

---
+++ HW5/HW5/motor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW5
+{
+    internal interface IMotor
+    {
+        void inputInfor();
+        void displayInfor();
+        void changeInfor();
+    }
+}
